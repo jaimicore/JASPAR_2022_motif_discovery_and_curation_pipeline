ceded32d5d651947f9b85c7a364eae787bdfc706
jaimicore <jcastro@lcg.unam.mx>
2020-08-14 09:11:25 +0200

    Aniseed motifs preprocessing completed

--- Aniseed/pre-process_scripts/Rename_Aniseed_motifs.R
+++ Aniseed/pre-process_scripts/Rename_Aniseed_motifs.R
@@ -3,9 +3,7 @@
 #####################
 required.libraries <- c("data.table",
                         "dplyr",
-                        "optparse",
-                        "tidyr",
-                        "UniprotR")
+                        "optparse")
 for (lib in required.libraries) {
   if (!require(lib, character.only = TRUE)) {
     install.packages(lib)
@@ -42,7 +40,9 @@ organism              <- "Ciona_robusta"
 
 ## Create output directory
 pfm.output.dir <- file.path(output.motif.folder, "PFMs")
-dir.create(output.motif.folder, showWarnings = F, recursive = T)
+logo.output.dir <- file.path(output.motif.folder, "Logos")
+dir.create(pfm.output.dir, showWarnings = F, recursive = T)
+dir.create(logo.output.dir, showWarnings = F, recursive = T)
 
 
 #############################################
@@ -68,6 +68,7 @@ colnames(aniseed.tab) <- c("TF_fam",
 ## Transform the interproID colums to one entry per line
 aniseed.tab <- aniseed.tab %>% 
   dplyr::filter(SELEX_clone != "")
+aniseed.tab$SELEX_clone <- substr(aniseed.tab$SELEX_clone, start = 1, stop = 5)
 
 ## New file with all motifs
 all.motifs.file <- file.path(pfm.output.dir, paste0("All_", organism, "_motifs_concatenated.tf"))
@@ -75,62 +76,70 @@ all.motifs.file <- file.path(pfm.output.dir, paste0("All_", organism, "_motifs_c
 TF.name.counter <- list()
 
 ## Iterate over each motif file
-for (f in list.files(input.motif.folder)[1:10]) {
+for (f in list.files(input.motif.folder)) {
   
   ## Get Selex probe ID
   selex.id <- gsub(f, pattern = "^([A-Za-z0-9]+)_\\d+.+$", replacement = "\\1")
+  selex.id <- substr(selex.id, start = 1, stop = 5)
+
   tf <- as.vector(subset(aniseed.tab, SELEX_clone == selex.id)$Gene_name)
+  tf <- unique(tf)
   
   ## Renamed problematic names
   ##
   ## Tbx2/3  -> Tbx2_3
   tf <- gsub(tf, pattern = "/", replacement = "-")
-  message("; Processing ", tf, " motif - SELEX probe: ", selex.id)
   
-  ## Count the number of times this TF appears in the file list
-  if (is.null(TF.name.counter[[tf]])) {
-    TF.name.counter[[tf]] <- NULL
-    TF.name.counter[[tf]] <- 1
+  ## Process the motif or skip
+  if (length(tf) == 0) {
+    message("; SELEX probe: ", selex.id, " not found in annotation table. Skip.")
   } else {
     
-    message("==================== ", TF.name.counter[[tf]])
-    TF.name.counter[[tf]] <- TF.name.counter[[tf]] + 1
-    message("++++++++++++++++++++ ", TF.name.counter[[tf]])
+    message("; Processing ", tf, " motif - SELEX probe: ", selex.id)
+
+    ## Count the number of times this TF appears in the file list
+    if (is.null(TF.name.counter[[tf]])) {
+      TF.name.counter[[tf]] <- NULL
+      TF.name.counter[[tf]] <- 1
+    } else {
+      TF.name.counter[[tf]] <- TF.name.counter[[tf]] + 1
+    }
+    
+
+    ## Set new motif file name
+    new.tmp.file <- file.path(pfm.output.dir, paste0(tf, "_pfm_", TF.name.counter[[tf]],".jaspar"))
+    tmp.files.list <- append(tmp.files.list, new.tmp.file)
+    
+    ## Add a header to the motifs
+    add.header.cmd <- paste0( '(echo ">',tf,'"_v', TF.name.counter[[tf]], ' && cat ', file.path(input.motif.folder, f), ') > ', new.tmp.file )
+    # message("; ", add.header.cmd)
+    system(add.header.cmd)
+    
+    
+    ## RSAT convert-matrix command
+    ## From cis-bp to transfac format
+    ## Add manually the TF name
+    new.tf.file <- file.path(pfm.output.dir, paste0(tf, "_pfm_", TF.name.counter[[tf]], ".tf"))
+    convert.matrix.cmd <- paste0("convert-matrix -i ", new.tmp.file, " -from jaspar -to tf -multiply 1000  > ", new.tf.file)
+    # message("; ", convert.matrix.cmd)
+    system(convert.matrix.cmd)
+    
+    ## Concat all motifs in a single file
+    cat.all.motifs.cmd <- paste0("cat ", new.tf.file, " >> ", all.motifs.file)
+    system(cat.all.motifs.cmd)
+    
+    ## Remove the jaspar temporal file
+    rm.tmp.file <- paste0("rm -rf ", new.tmp.file)
+    system(rm.tmp.file)
   }
-  
-  
-  
-  
-  ## Set new motif file name
-  new.tmp.file <- file.path(pfm.output.dir, paste0(tf, "_pfm.jaspar"))
-  tmp.files.list <- append(tmp.files.list, new.tmp.file)
-  
-  ## Add a header to the motifs
-  add.header.cmd <- paste0( '(echo ">',tf,'" && cat ', file.path(input.motif.folder, f), ') > ', new.tmp.file )
-  # message("; ", add.header.cmd)
-  system(add.header.cmd)
-  
-  
-  ## RSAT convert-matrix command
-  ## From cis-bp to transfac format
-  ## Add manually the TF name
-  new.tf.file <- file.path(pfm.output.dir, paste0(tf, "_pfm.tf"))
-  convert.matrix.cmd <- paste0("convert-matrix -i ", new.tmp.file, " -from jaspar -to tf -multiply 1000  > ", new.tf.file)
-  # message("; ", convert.matrix.cmd)
-  system(convert.matrix.cmd)
-  
-  ## Concat all motifs in a single file
-  cat.all.motifs.cmd <- paste0("cat ", new.tf.file, " >> ", all.motifs.file)
-  system(cat.all.motifs.cmd)
-  
-  ## Remove the jaspar temporal file
-  rm.tmp.file <- paste0("rm -rf ", new.tmp.file)
-  system(rm.tmp.file)
 }
 
 
 ## Generate logos
-logos.cmd <- paste("convert-matrix -i ", all.motifs.file, " -from tf -to tf -return logo -logo_dir ", logo.folder)
+logos.cmd <- paste("convert-matrix -i ", all.motifs.file, " -from tf -to tf -return logo -logo_dir ", logo.output.dir)
 message("; ", logos.cmd)
 system(logos.cmd)
+
+
+
 

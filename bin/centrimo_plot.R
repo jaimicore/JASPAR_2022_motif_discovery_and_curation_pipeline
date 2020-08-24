8a9174707961b05c79ee33f6940d5ddfdcdbc27a
jaimicore <jcastro@lcg.unam.mx>
2020-08-24 15:38:16 +0200

    Export centrality plot as png

--- bin/centrimo_plot.R
+++ bin/centrimo_plot.R
@@ -9,3 +9,14 @@ h = hist(((sites$V8 + sites$V9) / 2) - 250, breaks=501, plot=F);
 plot(h$mids, h$counts, type='l', main='', 
      xlab="Distance to peak centre", ylab="Number of motif occurrences");
 null = dev.off();
+
+
+outfile.png <- outfile
+outfile.png <- gsub(outfile.png, pattern = "\\.pdf$", replacement = "")
+outfile.png <- gsub(outfile.png, pattern = "\\.", replacement = "_")
+outfile.png <- paste0(outfile.png, ".png")
+png(outfile.png);
+h = hist(((sites$V8 + sites$V9) / 2) - 250, breaks=501, plot=F);
+plot(h$mids, h$counts, type='l', main='', 
+     xlab="Distance to peak centre", ylab="Number of motif occurrences");
+null = dev.off();

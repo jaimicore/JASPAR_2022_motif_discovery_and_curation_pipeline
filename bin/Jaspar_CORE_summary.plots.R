12b9884eca5664df314614e405a8fe721c76fa2f
jaimicore <jcastro@lcg.unam.mx>
2021-08-27 15:06:26 +0200

    Adapted to run with CORE and UNVALIDATED collections

--- bin/Jaspar_CORE_summary.plots.R
+++ bin/Jaspar_CORE_summary.plots.R
@@ -7,7 +7,8 @@ required.libraries <- c("data.table",
                         "optparse",
                         "plotly",
                         "RColorBrewer",
-                        "reshape2")
+                        "reshape2",
+                        "tidyr")
 
 for (lib in required.libraries) {
   if (!require(lib, character.only = TRUE)) {
@@ -41,8 +42,9 @@ results.dir               <- opt$output_directory
 motifs.per.taxon.tab.file <- opt$input_table
 plotly.export             <- as.numeric(opt$plotly_server)
 # motifs.per.taxon.tab.file <- "/home/jamondra/Downloads/Motifs_per_taxon_per_release.txt"
-# results.dir <- "/home/jamondra/Downloads/JASPAR_2020_plots"
+# results.dir               <- "/home/jamondra/Downloads/JASPAR_2022_plots"
 
+dir.create(results.dir, showWarnings = F, recursive = T)
 
 ##########################################
 ## Read JASPAR motifs per release table ##
@@ -50,104 +52,161 @@ plotly.export             <- as.numeric(opt$plotly_server)
 motifs.per.taxon.tab <- fread(motifs.per.taxon.tab.file)
 
 ## Add column with the total number of motifs
-motifs.per.taxon.tab$All_taxa <- rowSums(motifs.per.taxon.tab[,c("Vertebrates", "Plants", "Insects", "Nematodes", "Fungi", "Urochordates")],na.rm = T)
+motifs.per.taxon.tab$All_taxa <- rowSums(motifs.per.taxon.tab[,c("Vertebrates", "Plants", "Insects", "Nematodes", "Fungi", "Urochordates", "Cnidaria", "Protozoa", "Trematodes")],na.rm = T)
 
 ## Get the year of the latest release
-release.year <- max(motifs.per.taxon.tab$Year)
+release.year <- max(motifs.per.taxon.tab$Year, na.rm = T)
 
 ## Convert the matrix in a data.frame
-nb.motfs.per.release <- data.frame(melt(motifs.per.taxon.tab[,c(3:9)]))
-colnames(nb.motfs.per.release) <- c("Taxon", "Nb_motifs")
+nb.motfs.per.release <- data.frame(melt(motifs.per.taxon.tab[,c(-2)], id.vars = c("Collection", "Year")))
+nb.motfs.per.release <- nb.motfs.per.release %>% 
+                          rename(Taxon     = variable,
+                                 Nb_motifs = value)
+                          
+nb.taxa <- length(as.vector(unique(nb.motfs.per.release$Taxon)))
 
 ## Add the year column
-nb.motfs.per.release$Year <- rep(motifs.per.taxon.tab$Year, 7)
+nb.motfs.per.release$Year <- rep(motifs.per.taxon.tab$Year, nb.taxa)
 nb.motfs.per.release$Year <- ordered(nb.motfs.per.release$Year, levels = as.vector(unique(nb.motfs.per.release$Year)))
 
 ## Order factors by number of motifs
-nb.motfs.per.release$Taxon <- factor(nb.motfs.per.release$Taxon, levels = as.vector(c("All_taxa", "Vertebrates", "Plants", "Fungi", "Insects", "Nematodes", "Urochordates")))
+nb.motfs.per.release$Taxon <- factor(nb.motfs.per.release$Taxon, levels = as.vector(c("All_taxa",
+                                                                                      "Vertebrates",
+                                                                                      "Plants",
+                                                                                      "Fungi",
+                                                                                      "Insects",
+                                                                                      "Nematodes",
+                                                                                      "Urochordates",
+                                                                                      "Cnidaria",
+                                                                                      "Protozoa",
+                                                                                      "Trematodes")))
 
 ## Define color palette
-taxon.colors <- colorRampPalette(brewer.pal(7, "Dark2"), space = "Lab")(length(unique(nb.motfs.per.release$Taxon)))
-cols <- c( "#666666", taxon.colors)
-names(cols) <- c("All_taxa", "Vertebrates", "Plants", "Fungi", "Insects", "Nematodes", "Urochordates")
-
-
-################
-## Line chart ##
-################
-nb.motfs.per.release.gg <- 
-  ggplot(nb.motfs.per.release, aes(x = Year, y = Nb_motifs, color = Taxon, group = Taxon)) +
-  geom_line(size = 2) +
-  geom_point(size = 4, shape = 21, fill = "white") + 
-  # scale_color_brewer(palette = "Dark2") +
-  scale_colour_manual(values = cols) +
-  theme_classic() +
-  labs(title = "JASPAR CORE data growth per release", y = "# Profiles", x = "Year of release")
-
-ggsave(plot     = nb.motfs.per.release.gg,
-       filename = file.path(results.dir, "Jaspar_growth_lines.pdf"),
-       width = 9.5, height = 7.5)
-
-nb.motfs.per.release.gg <- ggplotly(nb.motfs.per.release.gg,
-                                    tooltip = c("y", "x", "group"))
-
-htmlwidgets::saveWidget(nb.motfs.per.release.gg, file.path(results.dir, "Jaspar_growth_lines.html"))
+taxon.colors <- colorRampPalette(brewer.pal(7, "Dark2"), space = "Lab")(nb.taxa - 1)
+cols         <- c( "#666666", taxon.colors)
+names(cols)  <- c("All_taxa",
+                  "Vertebrates",
+                  "Plants",
+                  "Fungi",
+                  "Insects",
+                  "Nematodes",
+                  "Urochordates",
+                  "Cnidaria",
+                  "Protozoa",
+                  "Trematodes")
 
 
 
+for (collection in c("CORE", "UNVALIDATED")) {
+  
+  message("; Plots for JASPAR ", collection, " collection")
+  
+  ################
+  ## Line chart ##
+  ################
+  nb.motfs.per.release.gg <- nb.motfs.per.release %>% 
+                                dplyr::filter(Collection == collection) %>% 
+                                drop_na(Year, Nb_motifs) %>% 
+                             ggplot(aes(x = Year, y = Nb_motifs, color = Taxon, group = Taxon)) +
+                                geom_line(size = 2) +
+                                geom_point(size = 4, shape = 21, fill = "white") + 
+                                scale_colour_manual(values = cols) +
+                                theme_classic() +
+                                labs(title = paste0("JASPAR ", collection, " data growth per release"), y = "# Profiles", x = "Year of release")
+                            
+  ggsave(plot     = nb.motfs.per.release.gg,
+         filename = file.path(results.dir, paste0("Jaspar_", collection, "_growth_lines.pdf")),
+         width    = 9.5,
+         height   = 7.5)
+  
+  nb.motfs.per.release.gg <- ggplotly(nb.motfs.per.release.gg,
+                                      tooltip = c("y", "x", "group"))
+  
+  htmlwidgets::saveWidget(nb.motfs.per.release.gg, file.path(results.dir, paste0("Jaspar_", collection, "growth_lines.html")))
+  message("; Line chart ready")
+  
+  
+  
+  ################
+  ## Donut plot ##
+  ################
+  jaspar.donut <- nb.motfs.per.release %>% 
+    dplyr::filter(Collection == collection) %>% 
+    drop_na(Year, Nb_motifs) %>% 
+    dplyr::filter(Year == release.year & Taxon != "All_taxa") %>% 
+    ggplot(aes(x = 2, y = Nb_motifs, fill = Taxon)) +
+    geom_bar(width = 1, stat = "identity") +
+    coord_polar("y", start = 0) +
+    xlim(0.5, 2.5) +
+    scale_fill_manual(values = cols) +
+    theme_void()
+  
+  ggsave(plot     = jaspar.donut,
+         filename = file.path(results.dir, paste0("Jaspar_", collection, "_", release.year,"_pie-donut.pdf")),
+         width    = 9.5,
+         height   = 7.5)
+  
+  ## Generate the donut plot
+  jaspar.donut <- 
+    nb.motfs.per.release %>% 
+    dplyr::filter(Collection == collection) %>% 
+    drop_na(Year, Nb_motifs) %>% 
+    dplyr::filter(Year == release.year & Taxon != "All_taxa") %>% 
+    group_by(Taxon) %>% 
+    plot_ly(labels = ~Taxon, values = ~Nb_motifs) %>%
+    add_pie(hole = 0.6) %>%
+    layout(title = paste0("JASPAR ", release.year, " ", collection),  showlegend = TRUE,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = TRUE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = TRUE), colorway = taxon.colors)
+  
+  htmlwidgets::saveWidget(jaspar.donut, file.path(results.dir, paste0("Jaspar_", collection, "_", release.year,"_pie-donut.html")))
+  message("; Donut chart ready")
+  
+  
+  ##############
+  ## Bar plot ##
+  ##############
+  jaspar.bars <- nb.motfs.per.release %>% 
+                    dplyr::filter(Taxon != "All_taxa") %>% 
+                    dplyr::filter(Collection == collection) %>% 
+                    drop_na(Year, Nb_motifs) %>% 
+                 ggplot(aes(x=Year, y=Nb_motifs, fill=Taxon)) +
+                    geom_bar(stat = "identity", position = "stack") +
+                    scale_fill_brewer(palette = "Dark2") +
+                    theme_classic() +
+                    theme(text = element_text(size = 15),
+                          axis.text.x = element_text(angle = 45, hjust = 1, size = 20),
+                          axis.text.y = element_text(hjust = 1, size = 20)) +
+                    labs(title = paste0("JASPAR ", collection, " data growth"), y = "# Profiles", x = "Year of release") +
+                    theme(plot.title = element_text(hjust = 0.5))
+  
+  
+  ggsave(plot     = jaspar.bars,
+         filename = file.path(results.dir, paste0("Jaspar_", collection, "_", release.year,"_growth_barplot.pdf")),
+         width    = 9.5,
+         height   = 7.5)
+  
+  jaspar.bars <- ggplotly(jaspar.bars,
+                          tooltip = c("y", "x", "fill"))
+  
+  
+  htmlwidgets::saveWidget(jaspar.bars, file.path(results.dir, paste0("Jaspar_", collection, "_", release.year,"_growth_barplot.html")))
+  message("; Bar chart ready")
+}
 
-################
-## Donut plot ##
-################
-jaspar.donut <- nb.motfs.per.release %>% 
-  dplyr::filter(Year == release.year & Taxon != "All_taxa") %>% 
-  ggplot(aes(x = 2, y = Nb_motifs, fill = Taxon)) +
-  geom_bar(width = 1, stat = "identity") +
-  coord_polar("y", start = 0) +
-  xlim(0.5, 2.5) +
-  scale_fill_brewer(palette = "Dark2") +
-  theme_void()
-
-ggsave(plot     = jaspar.donut,
-       filename = file.path(results.dir, paste0("Jaspar_", release.year,"_pie-donut.pdf")),
-       width    = 9.5,
-       height   = 7.5)
-
-## Generate the donut plot
-jaspar.donut <- 
-  nb.motfs.per.release %>% 
-  dplyr::filter(Year == release.year & Taxon != "All_taxa") %>% 
-  group_by(Taxon) %>% 
-  plot_ly(labels = ~Taxon, values = ~Nb_motifs) %>%
-  add_pie(hole = 0.6) %>%
-  layout(title = paste0("JASPAR ", release.year, " CORE profile stats"),  showlegend = TRUE,
-         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = TRUE),
-         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = TRUE), colorway = taxon.colors)
-
-htmlwidgets::saveWidget(jaspar.donut, file.path(results.dir, "Jaspar_growth_lines.html"))
 
 
 
-##############
-## Bar plot ##
-##############
-jaspar.bars <- nb.motfs.per.release %>% 
-  dplyr::filter(Taxon != "All_taxa") %>% 
-  ggplot(aes(x=Year, y=Nb_motifs, fill=Taxon)) +
-    geom_bar(stat = "identity", position = "stack") +
-    scale_fill_brewer(palette = "Dark2") +
-    theme_classic() +
-    theme(text = element_text(size = 15),
-          axis.text.x = element_text(angle = 45, hjust = 1, size = 20),
-          axis.text.y = element_text(hjust = 1, size = 20)) +
-    labs(title = "JASPAR CORE data growth", y = "# Profiles", x = "Year of release") +
-    theme(plot.title = element_text(hjust = 0.5))
 
 
-ggsave(plot     = jaspar.bars,
-       filename = file.path(results.dir, paste0("Jaspar_", release.year,"_growth_barplot.pdf")),
-       width    = 9.5,
-       height   = 7.5)
+
+
+
+
+
+
+
+
 
 
 

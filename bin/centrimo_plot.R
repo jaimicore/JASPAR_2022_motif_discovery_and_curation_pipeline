7c71854ca27fb07159dbfddad0ab63f9c88d7cb3
jaimicore <jcastro@lcg.unam.mx>
2021-10-25 10:49:57 +0200

    Added messages to ease debug

--- bin/centrimo_plot.R
+++ bin/centrimo_plot.R
@@ -9,6 +9,7 @@ h = hist(((sites$V8 + sites$V9) / 2) - 250, breaks=501, plot=F);
 plot(h$mids, h$counts, type='l', main='', 
      xlab="Distance to peak centre", ylab="Number of motif occurrences");
 null = dev.off();
+message("; Exported centrimo plot PDF format")
 
 
 outfile.png <- outfile
@@ -20,3 +21,4 @@ h = hist(((sites$V8 + sites$V9) / 2) - 250, breaks=501, plot=F);
 plot(h$mids, h$counts, type='l', main='', 
      xlab="Distance to peak centre", ylab="Number of motif occurrences");
 null = dev.off();
+message("; Exported centrimo plot png format")
